6015bf67ed7c74fe42efd5817481bb189081efbf
Zeynep <pamukzeynep361@gmail.com>
2025-04-22 14:23:28 +0300

    degisiklij yapildi 14.23

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
     path("register/", RegisterView.as_view(), name="register"),
-    path("login/", TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/", TokenObtainPairView.as_view(), name="get_token"),
     path("refresh/", TokenRefreshView.as_view(), name="token_refresh"),
     path("profile/", ProfileView.as_view(), name="user-profile"),
     path("change-password/", ChangePasswordView.as_view(), name="change-password")
