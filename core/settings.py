cf7a416e107a4c6d800888a3a7cd95251d23c8b4
Zeynep <pamukzeynep361@gmail.com>
2025-05-09 17:50:23 +0300

    09.05

--- core/settings.py
+++ core/settings.py
@@ -153,6 +153,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
 
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
